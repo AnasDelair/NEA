f8ae8195eca20f12d2d586ea543362e4f9e55e74
AnasDelair <24Adelair@heckgrammar.co.uk>
2025-12-01 09:59:00 +0000

    database login error

--- warehouse_system/modules/database.py
+++ warehouse_system/modules/database.py
@@ -1,4 +1,5 @@
 from mysql.connector import pooling as pool
+from mysql.connector import connect
 from dotenv import load_dotenv
 from os import environ as env
 
@@ -6,13 +7,30 @@ load_dotenv()
 
 class Database:
     def __init__(self):
-        self.pool = pool.MySQLConnectionPool(
-            pool_name="database",
-            pool_size=5,
-            pool_reset_session=True,
-            host=env.get("DATABASE_HOST"),
-            user=env.get("DATABASE_USER"),
-            password=env.get("DATABASE_PASS"),
-            database=env.get("DATABASE")
-        )
-        print(self.pool.get_connection())
+        #self.pool = pool.MySQLConnectionPool(
+        #    pool_name="database",
+        #    pool_size=5,
+        #    pool_reset_session=True,
+        #    host=env.get("DATABASE_HOST"),
+        #    user=env.get("DATABASE_USER"),
+        #    password=env.get("DATABASE_PASS"),
+        #    database=env.get("DATABASE")
+        #)
+        
+        # print(self.pool.get_connection())
+
+        context = connect(
+            user="u381396247_adelair", 
+            password="I&ac@aCp&0", 
+            host="srv1475.hstgr.io", 
+            port="3306", 
+            database="u381396247_adelair"
+            )
+        
+        print("connected")
+        
+        context.close()
+
+if __name__ == "__main__":
+    print("running")
+    db = Database()
